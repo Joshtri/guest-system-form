83b18f5edfcbeb3d6a74d3c10d142274b703a739
iqrom25 <iqromprasetyo@payfirstsolutions.com>
2024-05-29 13:13:42 +0700

    feat: coverted signature to base64 and save it to DB

--- models/guest.model.js
+++ models/guest.model.js
@@ -1,47 +1,50 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const GuestSchema = new mongoose.Schema({
-
+const GuestSchema = new mongoose.Schema(
+  {
     nama_lengkap: {
-        type : String, 
-        required : true,
-        
+      type: String,
+      required: true,
     },
 
     //asal instansi.
-    asal : {
-        type: String,
-        required: true,
-        // unique: true
+    asal: {
+      type: String,
+      required: true,
+      // unique: true
     },
 
-    keperluan : {
-        type : String, 
-        required : true,
+    keperluan: {
+      type: String,
+      required: true,
     },
 
-    orang_dituju : {
-        type : String, 
-        required : true,
+    orang_dituju: {
+      type: String,
+      required: true,
     },
 
-    no_hp : {
-        type : String, 
-        required : true,
+    no_hp: {
+      type: String,
+      required: true,
     },
 
     imageData: {
-        type : String, 
-        // required : true,
-        
+      type: String,
+      // required : true,
+    },
+
+    signature: {
+      type: String,
+      required: true,
     },
     // role: {
     //     type: String,
     //     enum: ['admin', 'manager'], // Hanya nilai 'admin' atau 'manager' yang diperbolehkan
     //     default: 'admin', // Nilai default jika tidak disediakan
     // },
-    
-}, {timestamps: true});
+  },
+  { timestamps: true }
+);
 
-
-export default mongoose.model('Guest', GuestSchema);
+export default mongoose.model("Guest", GuestSchema);
